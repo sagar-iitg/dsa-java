ce6b1c42c25f61a4f6d5e7d08c3ef004b3999a85
sagar <sagar-iitg>
2023-01-07 11:26:40 +0530

    patterns

---
+++ src/com/sagar/misc/Test.java
@@ -0,0 +1,17 @@
+package com.sagar.misc;
+
+import java.sql.SQLOutput;
+
+public class Test
+{
+
+    public static void main(String[] args) {
+        String s="hello";
+        String  s1 = "dog cat cat dog";
+        System.out.println(s.length());
+        System.out.println(s1.split(" ").length);
+
+    }
+
+
+}
---
+++ src/com/sagar/patterns/questions/Patterns.java
@@ -0,0 +1,85 @@
+package com.sagar.patterns.questions;
+
+public class Patterns {
+
+
+    public static void main(String[] args)
+    {
+
+        //pattern2(4);
+        pattern4(5);
+
+    }
+
+
+    private static void pattern4(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            //for every row run the cols
+            for(int col=1;col<=row;col++)
+            {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+
+    private static void pattern3(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            //for every row run the cols
+            for(int col=1;col<=n-row+1;col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+
+
+    private static void pattern2(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            //for every row run the cols
+            for(int col=1;col<=row;col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+
+        }
+
+
+    }
+
+    private static void pattern1(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            //for every row run the cols
+            for(int col=1;col<=n;col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+
+
+}
