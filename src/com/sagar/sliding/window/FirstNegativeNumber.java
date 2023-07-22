92d2f7ddcfb91e45eff215d5da208a23aab4cc7f
Sagar Kumar <sagar-iitg>
2023-07-22 10:41:57 +0530

    First Negative number

--- src/com/sagar/sliding/window/FirstNegativeNumber.java
+++ src/com/sagar/sliding/window/FirstNegativeNumber.java
@@ -5,9 +5,17 @@ public class FirstNegativeNumber {
 
     public static void main(String[] args) {
 
-        int arr[]={100, 200, 300, 400};
+        int[] arr={100, 200, 300, 400};
         int k=2;
         int ans=solve(arr,k);
         System.out.println(ans);
     }
+
+    private static int solve(int[] arr, int k) {
+
+
+        return 0;
+
+    }
+
 }
