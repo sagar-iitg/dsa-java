c7840bc3f8036b4d94c80d4bb5c9455973c76602
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-08-08 19:03:47 +0530

    Create CheckEnglishCharacter.java

---
+++ src/com/sagar/string/CheckEnglishCharacter.java
@@ -0,0 +1,35 @@
+package com.sagar.string;
+
+public class CheckEnglishCharacter {
+
+
+    public static void main(String[] args) {
+
+        String s="abc-";
+        for(char ch:s.toCharArray())
+        {
+            boolean ans=checkEnglishCharcater(ch);
+            System.out.println(ans);
+
+        }
+
+        System.out.println((int)'z');
+
+    }
+
+    private static boolean checkEnglishCharcater(char ch) {
+
+
+
+            if(ch>=55 && ch<=90)
+            {
+                return true;
+            }
+            if(ch>=97 && ch<=122)
+            {
+                return true;
+            }
+
+        return  false;
+    }
+}
