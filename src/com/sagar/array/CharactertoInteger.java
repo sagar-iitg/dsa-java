4f6abcae6f2d5bc6ce098e2ee7134210048bb4ef
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-09-05 21:56:40 +0530

    powerset

---
+++ src/com/sagar/array/CharactertoInteger.java
@@ -0,0 +1,14 @@
+package com.sagar.array;
+
+public class CharactertoInteger {
+
+    public static void main(String[] args) {
+
+//        char[] ch={'9','1','2','3'};
+//        int x=ch[0]-'0';
+//        System.out.println(((Object) (ch[0]-'0')).getClass().getName() );
+
+        System.out.println(Character.isDigit('1'));
+
+    }
+}
---
+++ src/com/sagar/array/Check.java
@@ -0,0 +1,15 @@
+package com.sagar.array;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Check {
+
+    public static void main(String[] args) {
+        List<Integer> ans=new LinkedList<>();
+        Queue<Integer> q=new LinkedList<>();
+
+
+    }
+}
