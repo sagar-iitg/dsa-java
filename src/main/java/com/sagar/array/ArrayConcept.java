83cc8f62bff38033948b87cac479cae96bc97648
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-02 23:46:06 +0530

    Create ArrayConcept.java

---
+++ src/main/java/com/sagar/array/ArrayConcept.java
@@ -0,0 +1,13 @@
+package com.sagar.array;
+
+import java.util.Arrays;
+
+public class ArrayConcept {
+
+    public static void main(String[] args) {
+        Integer[] ans=new Integer[10];
+        System.out.println(Arrays.toString(ans));
+        Boolean[][] b=new Boolean[2][3];
+        System.out.println(Arrays.deepToString(b));
+    }
+}
