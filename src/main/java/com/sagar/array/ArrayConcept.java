932d36de7b0cf2451cce599a5d79ae035be2820b
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-09 14:34:58 +0530

    Update ArrayConcept.java

--- src/main/java/com/sagar/array/ArrayConcept.java
+++ src/main/java/com/sagar/array/ArrayConcept.java
@@ -1,6 +1,7 @@
 package com.sagar.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ArrayConcept {
 
@@ -9,5 +10,11 @@ public class ArrayConcept {
         System.out.println(Arrays.toString(ans));
         Boolean[][] b=new Boolean[2][3];
         System.out.println(Arrays.deepToString(b));
+
+        //hashmap concept
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(1,10);
+        map.put(2,20);
+        System.out.println(map.get(3));
     }
 }
