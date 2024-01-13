0d5c458acb3b85aa1d75cb11cfe9a48e9dd43db0
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-13 23:22:59 +0530

    Update FreCount.java

--- src/main/java/com/sagar/array/FreCount.java
+++ src/main/java/com/sagar/array/FreCount.java
@@ -6,12 +6,13 @@ public class FreCount {
     public static void main(String[] args) {
 
 
-        String s="abcab";
-        int[] count=new int[26];
+        String s="! abcab ";
+        int[] count=new int[128];
         for(int i=0;i<s.length();i++){
 
-            count[s.charAt(i)-'a']++;
+            count[(int)s.charAt(i)]++;
         }
         System.out.println(Arrays.toString(count));
+        System.out.println(count.length);
     }
 }
