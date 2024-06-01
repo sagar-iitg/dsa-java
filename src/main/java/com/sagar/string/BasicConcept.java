ca0f034872b93903e897ff28c2b268d76afb1db4
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-01 11:17:28 +0530

    Create BasicConcept.java

---
+++ src/main/java/com/sagar/string/BasicConcept.java
@@ -0,0 +1,12 @@
+package com.sagar.string;
+
+public class BasicConcept {
+
+    public static void main(String[] args) {
+        String str="check";
+        String result="";
+        result=str;
+        //System.out.println(result);
+        System.out.println(str.substring(2,2).isBlank());
+    }
+}
