43f8b50c6c12afa9fe5faa2f9430e1bb99e94810
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-13 20:07:07 +0530

    Update CharactertoInteger.java

--- src/main/java/com/sagar/array/CharactertoInteger.java
+++ src/main/java/com/sagar/array/CharactertoInteger.java
@@ -1,15 +1,12 @@
 package com.sagar.array;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class CharactertoInteger {
 
     public static void main(String[] args) {
 
-//        char[] ch={'9','1','2','3'};
-//        int x=ch[0]-'0';
-//        System.out.println(((Object) (ch[0]-'0')).getClass().getName() );
+        char[] ch={'9','1','2','3'};
+        int x=ch[0]-'0';
+        System.out.println(((Object) (ch[0]-'0')).getClass().getName() );
 
         System.out.println(Character.isDigit('1'));
 
