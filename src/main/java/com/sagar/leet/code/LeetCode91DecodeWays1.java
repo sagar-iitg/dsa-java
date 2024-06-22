55019b10cdb6a8724ab64b3740d33b73bb6e5fa1
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-22 16:09:49 +0530

    Create LeetCode91DecodeWays1.java

---
+++ src/main/java/com/sagar/leet/code/LeetCode91DecodeWays1.java
@@ -0,0 +1,13 @@
+package com.sagar.leet.code;
+
+public class LeetCode91DecodeWays1 {
+
+    public static void main(String[] args) {
+        String str="127";
+
+
+    }
+    private static void recursiveDecode(){
+
+    }
+}
