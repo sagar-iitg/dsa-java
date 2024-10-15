5cd872b83194345c298e1c46f2bfe3132b5257d5
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-10-15 16:58:17 +0530

    Update BuildHeap.java

--- src/main/java/com/sagar/heap/BuildHeap.java
+++ src/main/java/com/sagar/heap/BuildHeap.java
@@ -14,9 +14,12 @@ public class BuildHeap {
     private static void buildMaxHeap(int[] arr,int n){
         //step down
         for(int i=(n/2)-1;i>=0;i--){
-            heapify(arr,i,n);
+            heapify1(arr,i,n);
+            //[30, 24, 20, 23, 19, 12, 18, 22, 14, 17, 15]
         }
     }
+
+    //recursion
     private static void heapify(int[] arr,int index,int n){
         int largest=index;
         int left=2*index+1;
@@ -26,13 +29,38 @@ public class BuildHeap {
         if( left<n && arr[left]> arr[largest]){
             largest=left;
         }
-        //for min heap change sign < 
+        //for min heap change sign <
         if( right <n && arr[right]>arr[largest]){
             largest=right;
         }
         if(largest!=index){
             swap(arr,largest,index);
             heapify(arr,largest,n);
+        }
+
+    }
+    //iterative
+    private static void heapify1(int[] arr,int index,int size){
+        while(index<size) {
+
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+
+            if (left < size && arr[largest] < arr[left]) {
+                largest = left;
+            }
+
+            if (right < size && arr[largest] < arr[right]) {
+                largest = right;
+            }
+
+            if (largest == index) {
+                return;
+            } else {
+                swap(arr, largest, index);
+                index = largest;
+            }
         }
 
     }
